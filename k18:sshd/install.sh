b09a1ead1db15607d4723de9c992736d6ce9a1a3
isx78745231 <isx78745231@i07.informatica.escoladeltreball.org>
2019-02-22 10:48:49 +0100

    kk

--- k18:sshd/install.sh
+++ k18:sshd/install.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 groupadd local01
-groupadd local02
+groupadd kusers
 useradd -g users -G local01 local01
 useradd -g users -G local01 local02
 useradd -g users -G local01 local03
-useradd -g users -G local02 local04
-useradd -g users -G local02 local05
-useradd -g users -G local02 local06
+useradd -g users -G kusers local04
+useradd -g users -G kusers local05
+useradd -g users -G kusers local06
 echo "local01" |passwd --stdin local01
 echo "local02" |passwd --stdin local02
 echo "local03" |passwd --stdin local03
-/usr/sbin/ssh-keygen -A
+/usr/bin/ssh-keygen -A
 cp /opt/docker/krb5.conf /etc/krb5.conf
